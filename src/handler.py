1572b8816f0992aeee2880e1c05b1db6d5ec0053
Asbjørn Apeland <aude@users.noreply.github.com>
2018-10-26 19:02:41 +0200

    add openfaas support

---
+++ src/handler.py
@@ -0,0 +1,11 @@
+def handle(req):
+    """handle a request to the function
+    Args:
+        req (str): request body
+    """
+
+    data = 'hello penguins!'
+
+    data += '\n\n' + req
+
+    return req
